0411408d27959ade0c35967aee327c2ad8f0f5e8
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-05-26 22:54:42 +0300

    Refactor/fix MultiDict.__setitem__, fix __call__
    +            mm = MultiMethod(key)
    +            oval = self[key]
    +            mm.register(oval)
    
         def __call__(self, *args, **kwds):
    -        bound = tuple((type(a) for a in args))

--- multidict.py
+++ multidict.py
@@ -8,18 +8,18 @@ import inspect
 
 class MultiDict(dict):
     def __setitem__(self, key, val):
-        if key.startswith("__") and key.endswith("__"):
+        if key not in self:
             super().__setitem__(key, val)
             return
-        if key in self:
-            mm = self[key]
-            if isinstance(mm, MultiMethod):
-                mm.register(val)
-            else:
-                mm = MultiMethod(key)
-                mm.register(val)
-            val = mm
-        super().__setitem__(key, val)
+        mm = self[key]
+        if isinstance(mm, MultiMethod):
+            mm.register(val)
+        else:
+            mm = MultiMethod(key)
+            oval = self[key]
+            mm.register(oval)
+            mm.register(val)
+            super().__setitem__(key, mm)
 
 
 class MultiMethod:
@@ -33,16 +33,18 @@ class MultiMethod:
     def __get__(self, instance, owner=None):
         if instance is None:
             return self
-        return types.MethodType(instance, self)
+        return types.MethodType(self, instance)
 
     def __call__(self, *args, **kwds):
-        bound = tuple((type(a) for a in args))
+        bound = tuple((type(a) for a in args[1:]))
         func = self.methods[bound]
         return func(*args, **kwds)
 
     def register(self, func: Callable):
         sig = inspect.signature(func)
-        _types = [parm.annotation for _, parm in sig.parameters.items()]
+        _types = [
+            parm.annotation for name, parm in sig.parameters.items() if name != "self"
+        ]
         self.methods[tuple(_types)] = func
 
 
@@ -50,24 +52,25 @@ class MultiMeta(type):
     def __new__(mcls, clsname, bases, clsdict):
         return super().__new__(mcls, clsname, bases, clsdict)
 
+    @classmethod
     def __prepare__(clsname, bases, clsdict):
         return MultiDict()
 
 
 class Calc(metaclass=MultiMeta):
     def compute(self, x: int, y: int):
-        pass
+        return x + y
 
     def compute(self, x: float, y: float = 6.7):  # noqa: F811
-        pass
+        return x + y
 
     def compute(self, x: str, y: str):  # noqa: F811
-        pass
+        return f"{x}-{y}"
 
 
 if __name__ == "__main__":
     c = Calc()
-    c.compute(10, 20)
-    c.compute(3.5, 6.7)
-    c.compute(3.6)
-    c.compute("Hello, ", "World!")
+    print(c.compute(10, 20))
+    print(c.compute(3.5, 6.7))
+    # c.compute(3.6)
+    print(c.compute("Hello, ", "World!"))
